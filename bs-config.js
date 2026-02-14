3433ca4c3ead4d90f863a79c2ffbf4c643a8e679
hanzzx311 <arroyanfarhan6@gmail.com>
2026-02-14 13:28:13 +0700

    Add public directory to file watch list

--- bs-config.js
+++ bs-config.js
@@ -1,6 +1,7 @@
+// bs-config.js
 module.exports = {
   proxy: "http://localhost:3001",
-  files: ["views/**/*.ejs"],
+  files: ["views/**/*.ejs", "public/**/*.*"], // reload otomatis jika file berubah
   port: 2000,
   ui: {
     port: 3005,
